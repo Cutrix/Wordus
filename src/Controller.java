004f6f4ae3981de107f5704897d84cfb93d06164
Cutrix <houessinonlandry@gmail.com>
2018-03-02 16:22:28 +0000

    un commit

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,13 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.web.HTMLEditor;
 
 public class Controller {
 
     @FXML
     private Label statusLbl;
+    @FXML
+    private HTMLEditor htmlEditor;
 
     //Fonction de selection de la langue
     @FXML
@@ -15,5 +18,10 @@ public class Controller {
     @FXML
     private void quitter() {
         System.exit(0);
+    }
+
+    @FXML
+    private void getAera() {
+        statusLbl.setText(htmlEditor.getHtmlText());
     }
 }

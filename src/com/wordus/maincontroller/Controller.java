b3156579b3481a143b1ef7e283eafbd9677b46bf
Unknown <houessinonlandry@gmail.com>
2018-03-13 17:19:55 +0000

    tab active

--- src/com/wordus/maincontroller/Controller.java
+++ src/com/wordus/maincontroller/Controller.java
@@ -4,10 +4,8 @@ import com.wordus.essentials.Dico;
 import com.wordus.essentials.SpellChecker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.FileChooser;
@@ -43,18 +41,19 @@ public class Controller {
 
     private Text text;
     private Label lb;
-    private TabPane currentTabs;
+    private Tab currentTabs;
 
     private final static int SEE_FROM = 3;
 
     private HTMLEditor htmlEditor1;
-
+    private Tab pan;
 
     // Menu gestion de saisi
     //Nouveau
     @FXML
     TabPane Tabpaner = new TabPane();
     int numTab = 2;
+    int nb = 0;
 
     @FXML
     private void NouveauTab() {
@@ -78,7 +77,7 @@ public class Controller {
         Tab tab1 = new Tab("Page " + numTab);
         tab1.contentProperty();
         Tabpaner.getTabs().addAll(tab1);
-      //  HTMLEditor htmlEditor0 = new HTMLEditor();
+      //  HTMLEditor htmlEditor1 = new HTMLEditor();
         tab1.setContent(htmlEditor1);
         numTab = numTab + 1;
 
@@ -133,20 +132,6 @@ private void SaveFile(String content, File file){
     }
 
 }
-
-@FXML
-private void tester() {
-     //System.out.println("je suis la ");
-        //System.out.println(htmlEditor.getHtmlText());
-        /*Document doc = Jsoup.parse(htmlEditor.getHtmlText());
-        Element content = doc.tagName("body p");
-        System.out.println(content.outerHtml());*/
-        Document doc = Jsoup.parseBodyFragment(htmlEditor.getHtmlText());
-        Element body = doc.body();
-        System.out.println(body);
-}
-
-
 
     //Quiter l'application
     @FXML
@@ -279,7 +264,9 @@ private void tester() {
     }
 
     public void Test(ActionEvent actionEvent) {
-        System.out.println(Tabpaner.getTabs().get(0));
+        currentTabs = Tabpaner.getTabs().get(Tabpaner.getSelectionModel().getSelectedIndex());
+        System.out.println(getCurrentTabs());
+        //System.out.println(Tabpaner.getSelectionModel().getSelectedIndex());
     }
 
     public void search(ActionEvent actionEvent) {
@@ -292,4 +279,9 @@ private void tester() {
 
         }
     }
+
+    public Tab getCurrentTabs() {
+        return currentTabs;
+    }
+
 }

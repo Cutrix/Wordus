f67d0350f2ba2e2e07db52797add3497b5dcb364
Unknown <houessinonlandry@gmail.com>
2018-03-14 15:57:11 +0000

    Exemple de test

--- src/com/wordus/tests/DicoTest.java
+++ src/com/wordus/tests/DicoTest.java
@@ -1,8 +1,14 @@
 package com.wordus.tests;
 
+import junit.framework.TestCase;
+
 import static org.junit.Assert.*;
 
-public class DicoTest {
-    
+public class DicoTest extends TestCase{
+
+    public void testDetectWord() {
+        String test = "Yoa";
+        assertTrue(test == "Yoa");
+    }
 
 }

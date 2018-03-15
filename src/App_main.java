a2d3f104b4f2f8450cec1cdace329bc74dac3382
Cutrix <houessinonlandry@gmail.com>
2018-03-15 12:17:36 +0000

    Redimensionnement des fenêtres

--- src/App_main.java
+++ src/App_main.java
@@ -4,6 +4,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
+import java.io.File;
+
 
 public class App_main extends Application{
 
@@ -17,11 +19,13 @@ public class App_main extends Application{
         primaryStage.setTitle("Wordus");
 		primaryStage.show();
 		//icon
-		primaryStage.getIcons().add(new Image("font\\icon\\text-editor.png"));
+		//primaryStage.getIcons().add(new Image("font\\icon\\text-editor.png"));
+		primaryStage.getIcons().add(new Image("font/icon/text-editor.png"));
 
 
 		//HtmlEditor css
 		//stage.getStylesheets().add(getClass().getResource("htmleditor.css").toExternalForm());
+
 
 		} catch(Exception e) {
 			e.printStackTrace();

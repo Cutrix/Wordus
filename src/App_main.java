aeabb2fa4fb53e599b00047a85a71c35b77e6b27
Alha13 <alhassaneo@gmail.com>
2018-03-20 20:56:18 +0000

    supression de import unitil

--- src/App_main.java
+++ src/App_main.java
@@ -1,10 +1,8 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.stage.StageStyle;
+import javafx.stage.Stage;
 
 
 

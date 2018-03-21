5e37645d24ee0a1dc514675cd037418ecc3d510d
Alha13 <alhassaneo@gmail.com>
2018-03-21 16:01:47 +0000

    MSG

--- src/App_main.java
+++ src/App_main.java
@@ -118,7 +118,7 @@ public class App_main extends Application {
 
                 }
                 Thread.sleep(400);
-                updateMessage("Bienvenu sur Wordus Text Editor !");
+                updateMessage("Bienvenu sur Wordus Text Editor !".);
 
 
                 return foundFriends;
